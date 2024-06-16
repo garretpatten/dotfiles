baffaf0b88ad2472357af9202b29523b20dca105
Garret Patten <garret.patten@proton.me>
2024-06-15 23:58:24 -0400

    Add more settings

--- nvim/lua/garret/set.lua
+++ nvim/lua/garret/set.lua
@@ -3,16 +3,6 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function() vim.opt_local.spell = true end
 })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
-  end
-})
-
-vim.cmd("colorscheme everforest")
 vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
 vim.opt.foldenable = true
@@ -22,6 +12,7 @@ vim.opt.re = 0
 vim.opt.showcmd = true
 vim.opt.showmatch = true
 vim.opt.showmode = true
+vim.opt.termguicolors = true
 vim.opt.wildmode = "longest,list"
 vim.cmd("syntax on")
 
@@ -32,17 +23,25 @@ vim.cmd("filetype indent on")
 
 -- Misc -----------------------------------------------------
 vim.g.loaded_perl_provider = 0
+vim.opt.backup = false
 vim.opt.backupdir = vim.fn.expand("~/.cache/vim")
 vim.opt.binary = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.encoding = "utf-8"
 vim.opt.compatible = false
+vim.opt.encoding = "utf-8"
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.updatetime = 50
 
 -- Navigation -----------------------------------------------------
+vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
 
 -- Searching -----------------------------------------------------
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.showmatch = true
 vim.opt.smartcase = true
@@ -53,5 +52,6 @@ vim.opt.expandtab = true
 vim.opt.listchars = { space = '·', tab = '=>' }
 vim.opt.list = true
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
